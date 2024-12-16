0512bfb4df33261e193932401118848f72125cce
Michele Melechì <mmelechi69gmail.com>
2024-12-16 16:07:03 +0100

    aìcalcolo area del rettangolo

---
+++ main.py
@@ -0,0 +1,7 @@
+print("""Questo programma calcola l'area di un rettangolo date base e altezza""")
+base = input("inserisci la base")
+
+altezza =  input("inserisci l'altezza")
+
+area = float(base) * float (altezza)
+print ("L'area del rettangolo è uguale " + str(area) )
